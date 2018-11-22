3389a915a0edc7b89e76fdce23cd5ed1c44fcdf6
kulouhuizhang <longlivegreatgermany@gmail.com>
2018-11-22 16:53:19 +0800

    Merge origin/master
    Conflicts:
    	index.php

--- signu/reg_sign.php
+++ signu/reg_sign.php
@@ -6,27 +6,40 @@
         exit("错误执行");
     }//判断是否有submit操作
 
-    $username=$_POST['name'];//post获取表单里的name
-    $password=$_POST['password'];//post获取表单里的password
-    //
+    $username=$_POST['reg_tel'];
+    $password=$_POST['reg_pas2'];
+    $tel=$_POST['reg_tel'];
+    
+   
     //数据存到session，初始化格式可能有点问题
-    session_start();
-    $_SESSION['username'] = $username;
-    $_SESSION['userid'] = $result['userid'];
 
-    include('connect.php');//链接数据库
-    $q="insert into user(id,username,password) values (null,'$name','$password')";//向数据库插入表单传来的值的sql
-    $reslut=mysql_query($q,$con);//执行sql
+
+$dbhost = 'localhost:3306'; // mysql服务器主机地址
+$dbuser = 'root'; // mysql用户名
+$dbpass = '@001xiaoshidaI'; // mysql用户名密码
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass);
+if(! $conn )
+{
+die('连接失败: ' . mysqli_error($conn));
+}
+echo '连接成功<br />';
+// 设置编码，防止中文乱码
+mysqli_query($conn , "set names utf8");
+
+mysqli_select_db($conn, 'test' );
+
+    $sql="insert into user(username,tel,password) values ('$username','$username','$password')";//向数据库插入表单传来的值的sql
     
-    if (!$reslut){
-        die('Error: ' . mysql_error());//如果sql执行失败输出错误
-    }else{
-        echo "注册成功";//成功输出注册成功
-    }
+    mysqli_select_db( $conn, 'test' );
+    $reslut=mysqli_query($conn,$sql);//执行sql
 
+if(!$reslut )
+{
+    die('无法插入数据: ' . mysqli_error($conn));
+}
+echo "数据插入成功\n";
+
+
+mysqli_close($conn);
     
-
-    mysql_close($con);//关闭数据库
-
-?>
-
+    ?>
