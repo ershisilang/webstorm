2bdb03f3f89d32dbdc6d363991886de36d7038c3
kulouhuizhang@126.com <longlivegreatgermany@gmail.com>
2018-11-29 00:49:44 +0800

    Merge branch 'master' of https://github.com/ershisilang/webstorm
    # Conflicts:
    #	.idea/workspace.xml

--- login.php
+++ login.php
@@ -4,72 +4,42 @@
 $username=$_POST['login_tel'];
 $password=$_POST['login_pas'];
 
+$dbhost = 'localhost:3306'; // mysql服务器主机地址
+$dbuser = 'root'; // mysql用户名
+$dbpass = '@001xiaoshidaI'; // mysql用户名密码
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass);
 
 
-$errmsg = '';
 
-if (!empty($username)) { // 用户填写了数据才执行数据库操作
+mysqli_query($conn , "set names utf8");
+mysqli_select_db($conn, 'test' );
+if ($username && $password){//如果用户名和密码都不为空
+   $sql = "SELECT * FROM user WHERE username='$username' AND password='$password'";//检测数据库是否有对应的username和password的sql
 
-//---------------------------------------------------------
 
-// 数据验证, empty()函数判断变量内容是否为空
+    $result=mysqli_query($conn,$sql);
+    $rows = mysqli_num_rows($result);
+    if($rows){//0 false 1 true
+        echo "<script language='javascript' type='text/javascript'>";
+        echo "window.location.href='./index_alreadylogin.html'";
+        echo "</script>";
 
-if (empty($username)) {
+    }else{
 
-$errmsg = '数据输入不完整';
+      echo "用户名或密码错误";
+
+
+    }
+
+
+}else{//如果用户名或密码有空
+    echo "请输入手机号或密码";
+
+
 
 }
 
-
-
-if(empty($errmsg)) { // $errmsg为空说明前面的验证通过
-
-// 调用mysqli的构造函数建立连接，同时选择使用数据库'test'
-
-$db = @new mysqli("127.0.0.1", "developer", "123456", "test");
-
-// 检查数据库连接
-
-if (mysqli_connect_errno()) {
-
-$errmsg = "数据库连接失败!
-
-\n";
-
-}
-
-else {
-
-// 查询数据库，看用户名及密码是否正确
-
-$sql = "SELECT * FROM user WHERE username='$username' AND password='$password'";
-
-$rs = $db->query($sql);
-
-// $rs->num_rows判断上面的执行结果是否含有记录，有记录说明登录成功 if ($rs && $rs->num_rows > 0) {
-
-// 在实际应用中可以使用前面提到的重定向功能转到主页
-
-$errmsg = "登录成功!";
-
-}
-
-else {
-
-$errmsg = "用户名或密码不正确，登录失败!";
-
-}
-
-
-
-// 关闭数据库连接
-
-$db->close();
-
-}
-
-}
-
-}
-
+mysqli_close($conn);
 ?>
+
+
