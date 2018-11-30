c9269a139a76ae7f9056017eafd56cfa68ee14d8
kulouhuizhang <longlivegreatgermany@gmail.com>
2018-11-30 17:27:49 +0800

    Merge branch 'master' of https://github.com/ershisilang/webstorm
    # Conflicts:
    #	.idea/workspace.xml

--- alertjsontest.php
+++ alertjsontest.php
@@ -1,7 +1,51 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Administrator
- * Date: 2018/11/29
- * Time: 9:33
- */
+// 从文件中读取数据到PHP变量
+$json_string = file_get_contents('./package.json');
+
+// 用参数true把JSON字符串强制转成PHP数组
+$data = json_decode($json_string, true);
+if(! $data )
+{
+    die('1: ' . mysqli_error($conn));
+}
+echo "2\n";
+
+$filewebid=$data->$id;
+$filestate=$data->$state;
+
+$dbhost = 'localhost:3306'; // mysql服务器主机地址
+$dbuser = 'root'; // mysql用户名
+$dbpass = '@001xiaoshidaI'; // mysql用户名密码
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass);
+
+/*
+if(! $conn )
+{
+die('连接失败: ' . mysqli_error($conn));
+}
+echo '连接成功<br />';
+// 设置编码，防止中文乱码
+*/
+mysqli_query($conn , "set names utf8");
+mysqli_select_db($conn, 'alert' );
+
+$sql="update alert set alert_audit_state=REPLACE (alert_audit_state,'ready','$filestate') where name=$filewebid";
+$retval = mysqli_query( $conn, $sql );
+
+
+
+if(! $retval )
+{
+    die('无法插入数据: ' . mysqli_error($conn));
+}
+echo "数据插入成功\n";
+mysqli_close($conn);
+
+
+
+
+?>
+
+
+
+
