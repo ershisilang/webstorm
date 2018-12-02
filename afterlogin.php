1b22afb3cd5aa988fe413b06d2703cf1326786df
kulouhuizhang@126.com <longlivegreatgermany@gmail.com>
2018-12-03 00:21:17 +0800

    Merge branch 'master' of https://github.com/ershisilang/webstorm
    # Conflicts:
    #	.idea/workspace.xml
    #	package.json
    #	price1.html

--- afterlogin.php
+++ afterlogin.php
@@ -1,12 +1,24 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['name'])) {
+
+    header("Location:price1.html");
+    exit;
+}
+?>
+
+
+
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8">
 
-    <title>爱通知产品价格 </title>
+    <title>爱通知控制面板</title>
 
     <meta name="keywords" content=" 爱通知产品价格 语音通知价格">
-    <meta name="description" content="爱通知通过电话通知的方法，提高电话提醒的效率，帮助个人通过电话提醒更好的管理生活，帮助企业通过电话通知提高信息发布效率">
+    <meta name="description" content=" 选择爱通知产品版本，选择语音通知功能">
 
 
     <script>
@@ -22,7 +34,10 @@
 
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script
+            src="https://code.jquery.com/jquery-3.3.1.js"
+            integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+            crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
@@ -42,20 +57,21 @@
         }
 
         .nav{font-size:
-                20px;
+            20px;
 
         }
 
         .nav-link{font-size:
-                20px;
+            20px;
         }
 
 
         .display-4 {
             font-size: 50px;
-
+            padding-top:0.5em;
             padding-bottom:0.5em;
         }
+
 
 
 
@@ -69,34 +85,23 @@
 
 
 
-
-
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand" href="#">爱通知</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
+    <div class="collapse navbar-collapse  justify-content-end"   id="navbarSupportedContent">
 
-            <li class="nav-item ">
-                <a class="nav-link" href="#"  onclick="javascript:window.location.href='index.html'">首页<span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item active">
-                <a class="nav-link" href="#"  onclick="javascript:window.location.href='price1.html'">产品价格</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#"   onclick="javascript:window.location.href='aboutus.html'">关于我们</a>
-            </li>
-        </ul>
         <ul class="nav navbar-nav navbar-right">
-            <li class="nav-item">
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Dropdown
+                </a>
+                <div class="dropdown-menu  dropdown-menu-right" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" id="my" href="#">退出</a>
 
-                <a data-toggle="modal" data-target="#register" href=""  class="nav-link"id="reg"><span class="glyphicon glyphicon-user"></span> 注册</a>
-            </li>
-            <li class="nav-item">
-                <a data-toggle="modal" data-target="#login" href=""  class="nav-link"><span class="glyphicon glyphicon-user"></span> 登陆</a>
+                </div>
             </li>
         </ul>
 
@@ -104,9 +109,8 @@
 </nav>
 
 
-
 <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-    <h1 class="display-4">产品价格</h1>
+    <h1 class="display-4" id="check">选择版本，立即使用</h1>
 
 </div>
 
@@ -143,6 +147,7 @@
 
 
 </div>
+
 
 
 
@@ -195,13 +200,23 @@
 
 
 
-
-
-<!-- Footer -->
-<div class="record text-center py-3">蜀ICP备18034123号 |  成都寻光者电子商务有限公司
-
-
-</div>
-
 </body>
 </html>
+
+
+<script type="text/javascript">
+
+    $(document).ready(function() {
+        $.ajax({
+            method: "post",
+            url: "session.php",
+            success: function(data)
+            {   $("#check").html(data);
+
+
+            }
+        });
+
+
+    });
+</script>
--- login.php
+++ login.php
@@ -13,6 +13,10 @@ $conn = mysqli_connect($dbhost, $dbuser, $dbpass);
 
 mysqli_query($conn , "set names utf8");
 mysqli_select_db($conn, 'test' );
+
+
+
+
 if ($username && $password){//如果用户名和密码都不为空
    $sql = "SELECT * FROM user WHERE username='$username' AND password='$password'";//检测数据库是否有对应的username和password的sql
 
@@ -20,16 +24,28 @@ if ($username && $password){//如果用户名和密码都不为空
     $result=mysqli_query($conn,$sql);
     $rows = mysqli_num_rows($result);
     if($rows){//0 false 1 true
+        $sql1 = "SELECT numcheck FROM user WHERE username='$username' AND password='$password' ";//检测次数是否用尽
+        $result1=mysqli_query($conn,$sql1);
+        $numcheck = mysqli_fetch_array($result);
 
-        //开启一个会话
+        $sql2 = "SELECT duecheck FROM user WHERE username='$username' AND password='$password'";//检测是否到期
+        $result2=mysqli_query($conn,$sql2);
+        $duecheck = mysqli_fetch_array($result2);
+
+
+        $sql3 = "SELECT is_member FROM user WHERE username='$username' AND password='$password'";
+        $result3=mysqli_query($conn,$sql3);
+        $ismember = mysqli_fetch_array($result3);
+        //开启一个会话,保存用户名（手机号)，剩余次数，到期时间
         session_start();
         $_SESSION['name'] = $username;
-
-
+        $_SESSION['numcheck']=$numcheck['numcheck'];
+        $_SESSION['duecheck']=$duecheck['duecheck'];
+        $_SESSION['ismember']= $ismember['is_member'];
 
 
         echo "<script language='javascript' type='text/javascript'>";
-        echo "window.location.href='./index_alreadylogin.php'";
+        echo "window.location.href='./afterlogin2.php'";
         echo "</script>";
 
     }else{
