992c9a818611d125f65b73a6775144866b18fc7a
kulouhuizhang <longlivegreatgermany@gmail.com>
2018-12-04 17:32:23 +0800

    Merge branch 'master' of https://github.com/ershisilang/webstorm
    # Conflicts:
    #	.idea/workspace.xml
    #	uploadactiontest.php

--- my.php
+++ my.php
@@ -1,7 +1,192 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Administrator
- * Date: 2018/12/4
- * Time: 11:27
- */
+
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+
+    <title>fd</title>
+
+    <meta name="keywords" content=" dfd">
+    <meta name="description" content=" fdfd">
+
+
+    <script>
+        var _hmt = _hmt || [];
+        (function() {
+            var hm = document.createElement("script");
+            hm.src = "https://hm.baidu.com/hm.js?24878ba02373ef0bb0a828acf30bb257";
+            var s = document.getElementsByTagName("script")[0];
+            s.parentNode.insertBefore(hm, s);
+        })();
+    </script>
+
+
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script
+        src="https://code.jquery.com/jquery-3.3.1.js"
+        integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+
+    <style type="text/css">
+
+
+        #reg{font-weight:bold;
+            color: #000000;
+
+        }
+
+        .navbar-brand{font-weight:bold;
+            font-size:30px;
+            color: #0084ff;
+
+        }
+
+        .nav{font-size:
+            20px;
+
+        }
+
+        .nav-link{font-size:
+            20px;
+        }
+
+
+        .container{
+
+            padding-top:2em;
+            padding-left:5em;
+
+        }
+
+
+
+
+
+
+
+
+
+    </style>
+
+</head>
+<body>
+
+
+
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Dropdown
+                </a>
+                <div class="dropdown-menu  dropdown-menu-right" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item"    href="session.php">logout</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+            </li>
+        </ul>
+
+    </div>
+</nav>
+
+
+<div class="container">
+<div class="about">
+
+    <p class="residue">fdfsfafdasfsdfsdfasdfsa</p>
+    <button type="button">Click Me!</button>
+    <p class="duetime">fdfsfafdasfsdfsdfasdfsa</p>
+
+    <p class="contact">fdfsfafdasfsdfsdfasdfsa</p>
+
+</div>
+
+</div>
+
+
+
+
+
+
+<!-- 注册窗口 -->
+<div id="register" class="modal fade" tabindex="-1">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-body">
+                <button class="close" data-dismiss="modal">
+                    <span>&times;</span>
+                </button>
+            </div>
+            <div class="modal-title">
+                <h2 class="text-center">title</h2>
+            </div>
+
+            <div id="textbox">
+                <p class="alignleft">Text on the left.</p>
+                <p class="alignright">Text on the right.</p>
+            </div>
+
+
+            <div id="textbox2">
+                <p class="alignleft">Text on the left.</p>
+                <p class="alignright">Text on the right.</p>
+            </div>
+
+            <div id="textbox3">
+                <p class="alignleft">Text on the left.</p>
+                <p class="alignright">Text on the right.</p>
+            </div>
+
+            <div class="modal-footer">
+                <button type="button" id="ordersumbit" class="btn btn-primary btn-lg btn-block">agree</button>
+            </div>
+
+
+
+
+
+
+
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+</body>
+</html>
+
+
+
+
